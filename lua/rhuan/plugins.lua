22b9d7989d90e27093ba6a2e26bc4a3f3114ef18
Rhuan Elias <rhuanelias29@gmail.com>
2026-07-09 12:56:48 -0300

    retirando copilot e pesos

--- lua/rhuan/plugins.lua
+++ lua/rhuan/plugins.lua
@@ -123,27 +123,6 @@ return {
     },
   },
   {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = {
-          enabled = true,
-          auto_trigger = true,
-          keymap = {
-            accept = "<C-y>",
-            next = "<M-]>",
-            prev = "<M-[>",
-            dismiss = "<C-n>",
-          },
-        },
-        panel = {
-          enabled = false,
-        },
-      })
-    end,
-  },
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     dependencies = {
@@ -191,54 +170,8 @@ return {
       require("mason").setup()
 
       require("mason-lspconfig").setup({
-        ensure_installed = {
-          "rust_analyzer",
-          "ts_ls",
-          "clangd",
-          "jdtls",
-          "solidity_ls",
-          "yamlls",
-        },
-        automatic_enable = false,
+        ensure_installed = {},
       })
-
-      vim.lsp.config("rust_analyzer", {
-        settings = {
-          ["rust-analyzer"] = {
-            checkOnSave = {
-              command = "clippy",
-            },
-            diagnostics = {
-              enable = true,
-              experimental = {
-                enable = true,
-              },
-            },
-          },
-        },
-      })
-
-      vim.lsp.config("ts_ls", {})
-      vim.lsp.config("clangd", {})
-      vim.lsp.config("yamlls", {})
-
-      vim.lsp.config("solidity_ls", {
-        cmd = { "solidity-language-server", "--stdio" },
-        filetypes = { "solidity" },
-        root_dir = function(bufnr)
-          return vim.fs.root(bufnr, {
-            "truffle-config.js",
-            "hardhat.config.js",
-            ".git",
-          })
-        end,
-      })
-
-      vim.lsp.enable("rust_analyzer")
-      vim.lsp.enable("ts_ls")
-      vim.lsp.enable("clangd")
-      vim.lsp.enable("yamlls")
-      vim.lsp.enable("solidity_ls")
     end,
   },
   {
