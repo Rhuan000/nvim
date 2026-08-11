4f40af8512317683eee55e17da35e027c9ee0316
Rhuan Elias <86079426+Rhuan000@users.noreply.github.com>
2026-08-11 10:08:22 -0300

    informando ao clang onde estão os headers para arquivos c

--- lua/rhuan/plugins/nvim-lspconfig/init.lua
+++ lua/rhuan/plugins/nvim-lspconfig/init.lua
@@ -1,62 +1,69 @@
 return {
-{
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-  },
-  config = function()
-    require("mason").setup()
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      require("mason").setup()
 
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "rust_analyzer",
-        "ts_ls",
-        "clangd",
-        "jdtls",
-        "solidity_ls",
-        "yamlls",
-      },
-      automatic_enable = false,
-    })
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "rust_analyzer",
+          "ts_ls",
+          "clangd",
+          "jdtls",
+          "solidity_ls",
+          "yamlls",
+        },
+        automatic_enable = false,
+      })
 
-    vim.lsp.config("rust_analyzer", {
-      settings = {
-        ["rust-analyzer"] = {
-          checkOnSave = {
-            command = "clippy",
-          },
-          diagnostics = {
-            enable = true,
-            experimental = {
+      vim.lsp.config("rust_analyzer", {
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              command = "clippy",
+            },
+            diagnostics = {
               enable = true,
+              experimental = {
+                enable = true,
+              },
             },
           },
         },
-      },
-    })
+      })
 
-    vim.lsp.config("ts_ls", {})
-    vim.lsp.config("clangd", {})
-    vim.lsp.config("yamlls", {})
+      vim.lsp.config("ts_ls", {})
 
-    vim.lsp.config("solidity_ls", {
-      cmd = { "solidity-language-server", "--stdio" },
-      filetypes = { "solidity" },
-      root_dir = function(bufnr)
-        return vim.fs.root(bufnr, {
-          "truffle-config.js",
-          "hardhat.config.js",
-          ".git",
-        })
-      end,
-    })
+      vim.lsp.config("clangd", {
+        cmd = {
+          "clangd",
+          "--query-driver=C:/msys64/ucrt64/bin/gcc.exe",
+        },
+      })
 
-    vim.lsp.enable("rust_analyzer")
-    vim.lsp.enable("ts_ls")
-    vim.lsp.enable("clangd")
-    vim.lsp.enable("yamlls")
-    vim.lsp.enable("solidity_ls")
-  end,
-}
+      vim.lsp.config("yamlls", {})
+
+      vim.lsp.config("solidity_ls", {
+        cmd = { "solidity-language-server", "--stdio" },
+        filetypes = { "solidity" },
+        root_dir = function(bufnr)
+          return vim.fs.root(bufnr, {
+            "truffle-config.js",
+            "hardhat.config.js",
+            ".git",
+          })
+        end,
+      })
+
+      vim.lsp.enable("rust_analyzer")
+      vim.lsp.enable("ts_ls")
+      vim.lsp.enable("clangd")
+      vim.lsp.enable("yamlls")
+      vim.lsp.enable("solidity_ls")
+    end,
+  },
 }
